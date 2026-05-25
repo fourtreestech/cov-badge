6e5f94a952647ca99dbc745264182b7f3b14100c
Neil Martin <neil@fourtrees.tech>
2026-05-25 09:57:46 +0100

    Read JSON file and extract coverage value

--- src/cov_badge/__init__.py
+++ src/cov_badge/__init__.py
@@ -1,7 +1,25 @@
+import json
+from typing import Any
+
 import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def main() -> None:
-    print("Hello from cov-badge!")
+    print("Loading JSON file...")
+    obj = load_json()
+    print(get_cov_percent(obj))
+
+
+def get_cov_percent(obj: dict[str, Any]) -> str:
+    """Get percentage coverage for badge display"""
+    return obj["totals"]["percent_statements_covered_display"]
+
+
+def load_json() -> dict:
+    """Load JSON file and return as a Python dict"""
+    with open("coverage.json") as file:
+        obj = json.load(file)
+    return obj
