5459b061fe2c264be1f0fc730e11935039d7f41e
Neil Martin <neil@fourtrees.tech>
2026-05-25 10:23:26 +0100

    Read and write README

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# cov-badge
+### cov-badge
+![coverage](https://img.shields.io/badge/coverage-91%25-green)
 
 *cov-badge* creates a coverage badge from a pytest-cov JSON file, and adds it to your project's README.
 
--- src/cov_badge/__init__.py
+++ src/cov_badge/__init__.py
@@ -10,12 +10,51 @@ app = typer.Typer()
 def main() -> None:
     print("Loading JSON file...")
     obj = load_json()
-    print(get_cov_percent(obj))
+    update_badge(get_cov_percent(obj))
 
 
-def get_cov_percent(obj: dict[str, Any]) -> str:
+def update_badge(coverage: int):
+    """Update the badge in the README"""
+    # Open README file
+    with open("README.md") as file:
+        readme_lines = file.readlines()
+
+    # Find the coverage line if there is one
+    index = 0
+    title = 0
+    last_badge = 0
+    for i, line in enumerate(readme_lines):
+        if line.startswith("![coverage]"):
+            index = i
+            break
+        elif line.startswith("###"):
+            title = i
+        elif line.startswith("!["):
+            last_badge = i
+
+    # If there isn't a coverage line already, insert one -
+    # after other badges if there are any, or just after the main title
+    else:
+        insert_after = last_badge if last_badge else title
+        index = insert_after + 1
+        readme_lines.insert(index, "")
+
+    # Update badge
+    readme_lines[index] = create_badge(coverage)
+
+    # Write README file
+    with open("README.md", mode="w") as file:
+        file.writelines(readme_lines)
+
+
+def create_badge(coverage: int) -> str:
+    """Create the badge script"""
+    return f"![coverage](https://img.shields.io/badge/coverage-{coverage}%25-green)\n"
+
+
+def get_cov_percent(obj: dict[str, Any]) -> int:
     """Get percentage coverage for badge display"""
-    return obj["totals"]["percent_statements_covered_display"]
+    return int(obj["totals"]["percent_statements_covered_display"])
 
 
 def load_json() -> dict:
